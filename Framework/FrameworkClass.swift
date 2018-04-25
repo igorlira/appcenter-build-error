6a26e97729c63b6e7d3c063d3f6862760d92453f
Igor Lira <igorlira@live.com>
2018-04-25 17:50:32 -0300

    initial commit

---
+++ Framework/FrameworkClass.swift
@@ -0,0 +1,15 @@
+//
+//  FrameworkClass.swift
+//  Framework
+//
+//  Created by Igor Lira on 4/25/18.
+//  Copyright © 2018 igorlira. All rights reserved.
+//
+
+import Foundation
+
+class Framework {
+    func noop() {
+        
+    }
+}
